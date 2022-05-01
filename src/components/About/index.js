0f339d49c3bd7ba317efd098bf7c994827b26022
alsornak <al.sorna01@gmail.com>
2022-05-01 12:59:00 +1000

    about

---
+++ src/components/About/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Sorna Kesavan</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="./assets/images/avatar.png" alt="Sorna Kesavan" />
+
+                <p>
+                Dedicated and focused customer-oriented professional excels at prioritizing, completing
+                multiple tasks simultaneously and following through to achieve project goals. Also adapt at
+                developing and maintaining database. 
+                </p>
+                <p>
+                Able to handle confidential information and identify
+                areas for system improvement with ease.
+                </p>
+                <p>
+                Successfully established effective systems for record retention by creating database for daily
+                correspondence tracking.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
