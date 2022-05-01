381741521977dd022f5d6b703170c697f5e502fc
alsornak <al.sorna01@gmail.com>
2022-05-01 14:54:27 +1000

    modified src

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer bg-dark fixed-bottom">
         <a href="https://github.com/alsornak"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
         <a href="https://www.linkedin.com/in/sorna-alagappan/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-        <a href="#"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" class="icon"/></a>
+        <a href="https://stackoverflow.com/"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" class="icon"/></a>
     </footer>
     )
 }
