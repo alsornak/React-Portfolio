a463cf8b7ffac67bf7c2ffdf86adfa6efa829bb0
alsornak <al.sorna01@gmail.com>
2022-05-03 02:37:18 +1000

    modified resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,6 +7,7 @@ function Resume() {
       <h2 className="top-title">Resume</h2>
       <hr></hr>
 
+      <div className="contenttext">
       <div>
         <div class="mt-5">
           <h2 className="top-title">Sorna Kesavan</h2>
@@ -37,14 +38,22 @@ function Resume() {
       <div class="justify-content-center mt-5">
         <div>
           <h2 className="top-title">Proficiencies</h2>
-          <ul>
-            <li>
-            HTML, CSS (Bootstrap, HandleBars), JavaScript, jQuery, React.js, IndexedDB,<br />
-            Node.js, Express.js, SQL, Sequelize, MongoDB, Mongoose, NoSQL, APIs, Templating.
-            </li>
-          </ul>
+          <div id="front-back" class="mt-3">
+                <h5><b>Front End Experience</b></h5>
+                <p>HTML, CSS (Bootstrap, Foundation, Bulma), JavaScript, jQuery, React.js, Angular.js, IndexedDB</p>
+            </div>
+    
+            <div id="front-back" class="mt-5">
+                <h5><b>Back End Experience</b></h5>
+                <p>
+                Node.js, Express.js, SQL (sqlite, mySQL, postgreSQL), Sequelize, NoSQL (MongoDB, Mongoose), Python, Django, API's (third-party, RESTful, server-side), Templating (Handlebars)
+                </p>
+            </div>
+          
         </div>
       </div>
+    </div>
+
     </section>
   );
 }
